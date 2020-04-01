4b0e46b84d733f8d1454be837cbe57c38d3d124f
Ben Collins <ben@bkcpartners.com>
2020-04-01 13:21:44 -0700

    Index.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,80 +1,35 @@
 <template>
-  <Layout :sidebar="false">
-    <div class="content">
-    </div>
+  <Layout>
+    <h1>
+      Uptracker Updates
+    </h1>
+     Some content.
   </Layout>
 </template>
 
-<static-query>
-query {
-  metadata {
-    siteName
-  }
-}
-</static-query>
-
 <script>
-import GitLink from '~/components/GitLink.vue'
-import Shortcut from '~/components/Shortcut.vue'
-
-export default {
-  components: {
-    GitLink,
-    Shortcut
-  },
-  data() {
-    return {
-      description: 'The ultimate static generated documentation theme for the JAM-stack'
-    }
-  },
-  metaInfo() {
-    return {
-      title: this.description,
-      meta: [
-        { key: 'description', name: 'description', content: 'A theme for static site documentation based on Gridsome, ready to deploy to Netlify in one click.' }
-      ]
-    }
-  }
-}
 </script>
 
+
 <style lang="scss" scoped>
-.content {
-  display: flex;
-  flex-direction: column;
+/deep/ > p {
+  opacity: .8;
 }
 
-h1 {
-  text-align: center;
-  max-width: 600px;
-  margin: 1.5em auto 1.5em;
+/deep/ > h2 {
+  padding-top: 100px;
+  margin-top: -80px;
 
   @include respond-above(md) {
-    max-width: 1000px;
+    font-size: 2rem;
   }
 }
 
-h2 {
-  font-size: 1.4rem;
-  margin: 0;
-}
-
-nav {
-  display: flex;
-  justify-content: space-between;
-  flex-direction: column;
-
-  @include respond-above(sm) {
-    flex-direction: row;
+/deep/ > p > img {
+    max-width: 100%;
   }
-}
 
-.git {
-  margin: 3em 0 0;
-  align-self: center;
-
-  @include respond-above(md) {
-    margin: 5em 0 0;
-  }
+.markdown {
+  padding-bottom: 50vh;
 }
 </style>
